459d86ccd5843084d1bc553894c20f8eac2a1c0e
Olivier Kannengieser <okannen@gmail.com>
2019-11-06 23:40:23 +0100

    cleaner

--- execution-start.hpp
+++ execution-start.hpp
@@ -2,10 +2,6 @@
 //c++ _start.S -static -fno-exceptions -fno-rtti -nostdlib -fno-threadsafe-statics
 #pragma once
 
-#include "stddef.h"
-#include <asm/unistd.h>
-#include <asm/prctl.h>
-
 extern "C"{
 using init_f_type  = void(*)(void);
 extern init_f_type __preinit_array_start[];
@@ -15,7 +11,7 @@ extern init_f_type __init_array_end[];
 extern init_f_type __fini_array_start[];
 extern init_f_type __fini_array_end[];
 }
-//
+
 namespace sys{
 
 inline void execute_init_array(){
@@ -41,35 +37,8 @@ struct auxv_t{
     void (*a_fnc)();
     } a_un;
   };
-#define BE_STRINGIFY(x) KAPI_STRINGIFY_(x)
-#define   BE_STRINGIFY_(x) #x
-inline int set_tls_buffer(void* buff){
-  register int ret asm("rax")=__NR_arch_prctl;
-  register int com asm("rdi")=ARCH_SET_FS;
-  register void* buff_ asm("rsi") = buff;
-  asm volatile(
-    //"movq $" BE_STRINGIFY(__NR_arch_prctl) ", %%rax\n\t"
-    //"movq $" BE_STRINGIFY(ARCH_SET_FS) ", %%rdi\n\t"
-    "syscall\n"
-    :"=r"(ret)
-    :"r"(buff),"r"(ret),"r"(com)
-    :"r11","rcx","r9","r8","r10","rdx","memory");
-  return ret;
-  }
-#undef BE_STRINGIFY
-#undef BE_STRINGIFY_
 }
 
-
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wmissing-declarations"
 extern "C"
-int execution_start [[gnu::used]]
+[[noreturn]] void execution_start [[gnu::used]]
  (char** argv_beg,char** argv_end,char** envp_beg,char** envp_end, sys::auxv_t* auxv_beg,sys::auxv_t* auxv_end);
-//  {
-//  execute_init_array();
-//  auto x=nsx_main(nsx::range_view(argv_beg,argv_end),nsx::range_view(envp_beg,envp_end),nsx::range_view(auxv_beg,auxv_end));
-//  execute_fini_array();
-//  return x;
-//  }
-#pragma GCC diagnostic pop
