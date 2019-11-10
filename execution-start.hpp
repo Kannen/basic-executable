5aee999548aa30b0c61d8932aabaec25c543df2e
Olivier Kannengieser <okannen@gmail.com>
2019-11-10 21:24:17 +0100

    working

--- execution-start.hpp
+++ execution-start.hpp
@@ -1,6 +1,7 @@
 //Documentation: psABI-x86_64, 3.4.1 Process Initialization/Initial Stack and Register State
 //c++ _start.S -static -fno-exceptions -fno-rtti -nostdlib -fno-threadsafe-statics
 #pragma once
+#include <linux/auxvec.h>
 
 extern "C"{
 using init_f_type  = void(*)(void);
@@ -30,15 +31,60 @@ inline void execute_fini_array(){
   }
 
 struct auxv_t{
-  size_t a_type;//int in the document but above it is specified as 2*8bytes
+  size_t a_type;
   union{
     long a_val;
     void* a_ptr;
     void (*a_fnc)();
-    } a_un;
+    };
   };
+
+struct envp_sentinel_t{
+  friend bool operator==(char** envp,envp_sentinel_t){
+    return *envp==nullptr;
+    }
+  friend bool operator==(envp_sentinel_t,char** envp){
+    return *envp==nullptr;
+    }
+  friend bool operator!=(char** envp,envp_sentinel_t){
+    return *envp!=nullptr;
+    }
+  friend bool operator!=(envp_sentinel_t,char** envp){
+    return *envp!=nullptr;
+    }
+  };
+inline constexpr auto envp_sentinel = envp_sentinel_t{};
+inline char** 
+get_envp(char ** argv_end){//ie argv+argc
+  return argv_end+1;
+  }
+
+struct auxv_t_sentinel_t{
+  friend bool operator==(auxv_t* aux,auxv_t_sentinel_t){
+    return aux->a_type==AT_NULL;
+    }
+  friend bool operator==(auxv_t_sentinel_t,auxv_t* aux){
+    return aux->a_type==AT_NULL;
+    }
+  friend bool operator!=(auxv_t* aux,auxv_t_sentinel_t){
+    return aux->a_type!=AT_NULL;
+    }
+  friend bool operator!=(auxv_t_sentinel_t,auxv_t* aux){
+    return aux->a_type!=AT_NULL;
+    }
+  };
+inline constexpr auto auxv_t_sentinel = auxv_t_sentinel_t{};
+
+inline auxv_t* 
+get_auxv_t(char ** envp_end){//envp_end==envp_sentinel
+  return reinterpret_cast<auxv_t*>(envp_end+1);
+  }
 }
 
 extern "C"
 [[noreturn]] void execution_start [[gnu::used]]
- (char** argv_beg,char** argv_end,char** envp_beg,char** envp_end, sys::auxv_t* auxv_beg,sys::auxv_t* auxv_end);
+  (int argc,char* argv[]);
+ //(char** argv_beg,char** argv_end
+ //,char** envp_beg,char** envp_end
+ //,sys::auxv_t* auxv_beg,sys::auxv_t* auxv_end
+ //,void(*atexit_rdx_function)(void));
