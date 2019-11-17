924c50da6922abea2c9173f291c2723cd00384f0
Olivier Kannengieser <okannen@gmail.com>
2019-11-17 22:44:52 +0100

    no gnu::used attribute

--- stdlib_replacement.cpp
+++ stdlib_replacement.cpp
@@ -1,13 +1,10 @@
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wmissing-declarations"
-extern "C" 
-void __stack_chk_fail [[using gnu:used,weak]] (void){
-  }
 extern "C"
-void __cxa_pure_virtual [[using gnu:used,weak]](void){
+void __cxa_pure_virtual [[gnu::weak]](void){
   }
 extern "C" 
-int strcmp [[using gnu:used,pure]](const char* x,const char* y){
+int strcmp [[gnu::pure]](const char* x,const char* y){
   int c=*x-*y;
   while (c==0 && *x!=0){
     ++x;++y;
@@ -16,13 +13,13 @@ int strcmp [[using gnu:used,pure]](const char* x,const char* y){
   return c;
   }
 extern "C" 
-unsigned long strlen [[using gnu:used,pure]](const char* x){
+unsigned long strlen [[gnu::pure]](const char* x){
   unsigned long c=0;
   while (*(x+c)!=0) ++c;
   return c;
   }
 extern "C" 
-void* memcpy [[using gnu:used,pure]](void* __restrict x,const void* __restrict y,long unsigned count){
+void* memcpy [[gnu::pure]](void* __restrict x,const void* __restrict y,long unsigned count){
   long unsigned i=0;
   while (i<count){
     static_cast<unsigned char*>(x)[i]=static_cast<const unsigned char*>(y)[i];
@@ -31,7 +28,7 @@ void* memcpy [[using gnu:used,pure]](void* __restrict x,const void* __restrict y
   return x;
   }
 extern "C" 
-void* memmove [[using gnu:used,pure]](void* x,const void* y,long unsigned count){
+void* memmove [[gnu::pure]](void* x,const void* y,long unsigned count){
   long unsigned i=0;
   while (i<count){
     static_cast<unsigned char*>(x)[i]=static_cast<const unsigned char*>(y)[i];
@@ -40,7 +37,7 @@ void* memmove [[using gnu:used,pure]](void* x,const void* y,long unsigned count)
   return x;
   }
 extern "C" 
-void* memset [[using gnu:used,pure]](void* x,int val,long unsigned count){
+void* memset [[gnu::pure]](void* x,int val,long unsigned count){
   auto c = static_cast<unsigned char>(val);
   long unsigned i=0;
   while (i<count){
